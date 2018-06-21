897725a4d3df7ff27eb30783d2af610dc2c5dadd
him4318 <31626936+him4318@users.noreply.github.com>
2018-06-21 23:29:47 +0530

    All is running

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -1,3 +1,5 @@
+
+import com.google.common.base.CharMatcher;
 import org.testng.annotations.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
@@ -13,94 +15,97 @@ import org.testng.annotations.BeforeClass;
 
 import java.util.List;
 import java.util.Properties;
-
-import javax.mail.*;
+import org.openqa.selenium.remote.RemoteWebElement;
+import org.testng.Assert;
 
 public class NewTest {
-	WebDriver driver;
 
-	public NewTest() {
-		// TODO Auto-generated constructor stub
+    WebDriver driver;
 
-	}
+    public NewTest() {
+        // TODO Auto-generated constructor stub
 
-	@Test
-	public void f() {
+    }
 
-		WebDriverWait wait = new WebDriverWait(driver, 30);
+    @Test
+    public void f() throws InterruptedException {
+        String count;
+        int before_count, after_count, n = 0;
 
-		// driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-		// driver.manage().window().maximize();
+        WebDriverWait wait = new WebDriverWait(driver, 30);
+        WebDriverWait inboxwait = new WebDriverWait(driver, 2);
+        
 
-		driver.get("https://mail.google.com/mail/u/0/#inbox");
+        // driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        // driver.manage().window().maximize();
+        driver.get("https://mail.google.com/mail/u/0/#inbox");
 
-		// gmail login
-		driver.findElement(By.id("identifierId")).sendKeys("himanshuqa4490");
-		driver.findElement(By.cssSelector("#identifierNext > content > span")).click();
-		wait.until(ExpectedConditions
-				.visibilityOfElementLocated(By.cssSelector("#password > div.aCsJod.oJeWuf > div > div.Xb9hP > input")));
-		driver.findElement(By.cssSelector("#password > div.aCsJod.oJeWuf > div > div.Xb9hP > input"))
-				.sendKeys("Himanshu@321#");
-		driver.findElement(By.cssSelector("#passwordNext > content")).click();
-		wait.until(ExpectedConditions
-				.visibilityOfElementLocated(By.tagName("tr")));	
-		
+        // gmail login
+        driver.findElement(By.id("identifierId")).sendKeys("USERNAME");
+        driver.findElement(By.cssSelector("#identifierNext > content > span")).click();
+        wait.until(ExpectedConditions
+                .visibilityOfElementLocated(By.cssSelector("#password > div.aCsJod.oJeWuf > div > div.Xb9hP > input")));
+        driver.findElement(By.cssSelector("#password > div.aCsJod.oJeWuf > div > div.Xb9hP > input"))
+                .sendKeys("PASSWORD");
+        driver.findElement(By.cssSelector("#passwordNext > content")).click();
+        wait.until(ExpectedConditions
+                .visibilityOfElementLocated(By.tagName("tr")));
 
-		JavascriptExecutor js=(JavascriptExecutor)driver;
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        RemoteWebElement next_page_button = (RemoteWebElement) js.executeScript("return document.querySelector(('img.amJ.T-I-J3'));");
+        System.out.println("opacity " + next_page_button.getCssValue("opacity"));
 
-		System.out.println("asd" + js.executeScript("return $('document').querySelector(('a'))[41].text;"));
+        if (!js.executeScript("return document.querySelector(('a.J-Ke.n0')).text;").equals("Inbox")) {
 
-		try {
-			
-			js.executeScript("return document.querySelector('tr.zA.zE').click();");
-			
-			
-		} catch (Exception e) {
-			
-			System.out.println("No unread emails");
-		}
-		
-		// driver.findElement(By.id("gb23")).click();
+            count = CharMatcher.digit().retainFrom(js.executeScript("return document.querySelector(('a.J-Ke.n0')).text;").toString());
+            before_count = Integer.parseInt(count);
+            System.out.println("before " + before_count);
+            while (n == 0) {
+                try {
+                    
+                    inboxwait.until(ExpectedConditions.jsReturnsValue("return document.querySelector('tr.zA.zE');"));
+                    js.executeScript("return document.querySelector('tr.zA.zE').click();");
+                    n = 1;
+                } catch (Exception e) {
+                    if (next_page_button.getCssValue("opacity").equals("0.55")) {
+                        next_page_button.click();
+                    } else {
+                        System.out.println("No unread emails and this is last page");
+                        n = 1;
+                    }
 
-		// // now talking un-read email form inbox into a list
-		// List<WebElement> unreademeil =
-		// driver.findElements(By.xpath("//*[@id=':3z']/span"));
-		// System.out.println("asd" + driver.findElement(By.cssSelector("#\3a 3z
-		// > span")).getText());
-		//
-		// // Mailer name for which i want to check do i have an email in my
-		// inbox
-		// String MyMailer = "Udacity";
-		//
-		// // real logic starts here
-		// for(int i=0;i<unreademeil.size();i++){
-		// if(unreademeil.get(i).isDisplayed()==true){
-		// // now verify if you have got mail form a specific mailer (Note
-		// Un-read mails)
-		// // for read mails xpath loactor will change but logic will remain
-		// same
-		// if(unreademeil.get(i).getText().equals(MyMailer)){
-		// System.out.println("Yes we have got mail form " + MyMailer);
-		// // also you can perform more actions here
-		// // like if you want to open email form the mailer
-		// break;
-		// }else{
-		// System.out.println("No mail form " + MyMailer);
-		// }
-		// }
-		//
-	}
+                }
 
-	@BeforeClass
-	public void afterClass() {
-		System.setProperty("webdriver.chrome.driver", "/home/himanshuchaudhary/Downloads/chromedriver");
-		driver = new ChromeDriver();
-	}
+            }
+            Thread.sleep(1000); //wait so that inbox count can be changed
+            count = CharMatcher.digit().retainFrom(js.executeScript("return document.querySelector(('a.J-Ke.n0')).text;").toString());
+            if (!count.isEmpty()) {
+                after_count = Integer.parseInt(count);
+            } else {
+                after_count = 0;
+            }
+            System.out.println("after " + after_count);
 
-	@AfterClass
-	public void afterTest() throws InterruptedException {
-		// Thread.sleep(10000);
-		driver.close();
-	}
+            if (after_count > before_count || after_count == before_count) {
+                Assert.fail("Inbox is not updated");
+            }
+
+        } else {
+            System.out.println("NO unread emails");
+        }
+
+    }
+
+    @BeforeClass
+    public void afterClass() {
+//		System.setProperty("webdriver.chrome.driver", "/home/himanshuchaudhary/Downloads/chromedriver");
+        driver = new ChromeDriver();
+    }
+
+    @AfterClass
+    public void afterTest() throws InterruptedException {
+        // Thread.sleep(10000);
+        driver.close();
+    }
 
 }
